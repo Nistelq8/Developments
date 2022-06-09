3cbe9bb3ead8db7af2144ef59f607a3495430bac
Abdulaziz Almarzouq <aziz.almarzouq@gmail.com>
2022-06-10 02:19:57 +0300

    Recreated flutter UI task

--- flutter_application_2/lib/main.dart
+++ flutter_application_2/lib/main.dart
@@ -14,18 +14,30 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.grey,
         body: SafeArea(
-          child: Center(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Container(
-                height: 100,
+          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+            Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(top: 20),
                 child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.asset("assets/images/vega.jpg")),
-              ),
-              Text("This is my newest Keyboard")
-            ]),
-          ),
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.asset("assets/images/vega.jpg"))),
+            Container(
+                color: Color.fromARGB(255, 64, 70, 66),
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.only(top: 100, right: 200),
+                child: Text(
+                  "This is my newest Keyboard",
+                  style: TextStyle(color: Colors.blue),
+                )),
+            Container(
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.only(right: 200),
+                child: Text("Its very expnesive")),
+            Container(
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.only(right: 200),
+                child: Text("It took soo long to arrive!"))
+          ]),
         ),
       ),
     );
