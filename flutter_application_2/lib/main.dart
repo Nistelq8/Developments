e41130a47f55a8aec98a3a7adf452d67508821e9
Abdulaziz Almarzouq <aziz.almarzouq@gmail.com>
2022-06-10 03:41:05 +0300

    Updated to match task Flutter Variables

--- flutter_application_2/lib/main.dart
+++ flutter_application_2/lib/main.dart
@@ -10,36 +10,40 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    String name = 'Vega';
+    int age = 1;
+    String type = 'Keyboard';
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.grey,
-        body: SafeArea(
-          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-            Container(
-                alignment: Alignment.center,
-                margin: EdgeInsets.only(top: 20),
-                child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Image.asset("assets/images/vega.jpg"))),
-            Container(
-                color: Color.fromARGB(255, 64, 70, 66),
-                alignment: Alignment.centerLeft,
-                margin: EdgeInsets.only(top: 100, right: 200),
-                child: Text(
-                  "This is my newest Keyboard",
-                  style: TextStyle(color: Colors.blue),
-                )),
-            Container(
-                alignment: Alignment.centerLeft,
-                margin: EdgeInsets.only(right: 200),
-                child: Text("Its very expnesive")),
-            Container(
-                alignment: Alignment.centerLeft,
-                margin: EdgeInsets.only(right: 200),
-                child: Text("It took soo long to arrive!"))
-          ]),
-        ),
+        home: Scaffold(
+      backgroundColor: Colors.grey,
+      body: SafeArea(
+        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+          Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(top: 20),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset("assets/images/vega.jpg"))),
+          Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.only(top: 100, left: 10),
+              child: Text(
+                "Name: $name",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              )),
+          Container(
+            alignment: Alignment.centerLeft,
+            margin: EdgeInsets.only(left: 10),
+            child: Text("Age: $age year",
+                style: TextStyle(fontWeight: FontWeight.bold)),
+          ),
+          Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.only(left: 10),
+              child: Text("Type: $type",
+                  style: TextStyle(fontWeight: FontWeight.bold)))
+        ]),
       ),
-    );
+    ));
   }
 }
