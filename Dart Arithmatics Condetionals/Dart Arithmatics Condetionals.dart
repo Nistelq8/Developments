20f4fbd41546bff1c25c238b0a2467619f2320ab
Abdulaziz Almarzouq <aziz.almarzouq@gmail.com>
2022-06-10 03:19:56 +0300

    Added Dart Arithmatics Condetionals and solved correctly

--- Dart Arithmatics Condetionals/Dart Arithmatics Condetionals.dart
+++ Dart Arithmatics Condetionals/Dart Arithmatics Condetionals.dart
@@ -1,21 +1,32 @@
 void main() {
+  String type = 'a';
   double tempInFahrenheit = 86;
-  int numberOfMark = 75;
-  temp(tempInFahrenheit);
-  mark(numberOfMark);
+  double tempInCelsius = 30;
+  int mark = 21;
+  temprature(tempInFahrenheit, type, tempInCelsius);
+  grade(mark);
+}
+//Fahrenheit to Celsius:   (°F − 32) / 1.8 = °C ---- The formula
+//Celsius to Fahrenheit: °C * 1.8000 + 32.00 = °F --- the formula
+
+void temprature(double tempInFahrenheit, String type, double tempInCelsius) {
+  if (type == 'F') {
+    print('${tempInFahrenheit}F = ${(tempInFahrenheit - 32) / 1.8}C');
+  } else {
+    print('${tempInCelsius}C ${(tempInCelsius * 1.8000) + 32.00}F');
+  }
 }
 
-void temp(double tempInFahrenheit) {
-  print('${tempInFahrenheit}F ${(tempInFahrenheit - 32) / 1.8}C');
-}
-
-//(°F − 32) / 1.8 = °C
-//86F = 30C
-
-void mark(int numberOfMark) {
-  if (numberOfMark >= 80) {
+void grade(int mark) {
+  if (mark >= 80) {
     print('A');
-  } else if (numberOfMark >= 70) {
+  } else if (mark >= 70 && mark < 80) {
     print('B');
+  } else if (mark >= 60 && mark < 70) {
+    print('C');
+  } else if (mark >= 50 && mark < 60) {
+    print('D');
+  } else {
+    print('F');
   }
 }
