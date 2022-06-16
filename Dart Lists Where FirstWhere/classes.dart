8bc767760c070a800f2823e8b213df7319dcbb5e
Abdulaziz Almarzouq <aziz.almarzouq@gmail.com>
2022-06-16 16:57:20 +0300

    added classes task

---
+++ Dart Lists Where FirstWhere/classes.dart
@@ -0,0 +1,11 @@
+class Chocolate {
+  String brand = "Kitkat";
+  int calories = 100;
+  double price = 1.2;
+  bool exp = false;
+}
+
+void main() {
+  Chocolate kitkat;
+  print(kitkat);
+}
---
+++ ducks/android/app/src/main/kotlin/com/example/ducks/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ducks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
